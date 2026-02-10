009ce98ace89e7c439e796ce55acc87eacc3e146
Okikiola Adedamola Akinyemi <jbdr1403@gmail.com>
2026-02-09 18:19:02 -0600

    Remove IAM access key outputs (avoid leaking secrets)

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,15 +14,6 @@ output "region" {
   value = "us-east-1"
 }
 
-output "bedrock_dev_view_access_key_id" {
-  value = aws_iam_access_key.bedrock_dev_view.id
-}
-
-output "bedrock_dev_view_secret_access_key" {
-  value     = aws_iam_access_key.bedrock_dev_view.secret
-  sensitive = true
-}
-
 output "assets_bucket_name" {
   value = aws_s3_bucket.assets.bucket
 }
